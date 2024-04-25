fa7c633c5a269b818d4a33220aa1af38d446e741
Pratiti Basu <104024345+coderpanda02@users.noreply.github.com>
2024-04-25 23:49:15 +0530

    Update Stack_array

--- Java_DS_Programs/src/testDSA/Stack_array.java
+++ Java_DS_Programs/src/testDSA/Stack_array.java
@@ -10,29 +10,51 @@ public class Stack_array {
 		top = -1;
 	}
 	
-	private void push(int i) {
+	private boolean isFull() {
 		if(top == max-1) {
+			return true;
+		}
+		return false;
+	}
+		
+	private boolean isEmpty() {
+		if(top<0) {
+			return true;
+		}
+		return false;
+	}
+	
+	private void push(int i) {
+		if(isFull()) {
 			System.out.println("Stack Overflow");
 		}
 		else {
 			stack[++top] = i;
 		}
 	}
-
+	
 	private int pop() { 
+		if(isEmpty()) {
+			System.out.println("Stack Underflow");
+			return 0;
+		}
 		return stack[top--]; //returning the deleted value then decrementing top
 	}
 
 	private int peek() { //peek at the top element
+		if(isEmpty()) {
+			return 0;
+		}
 		return stack[top];
 	}
 
+
 	private void show() {
-		for(int i=0; i<=top; i++) {
-			System.out.print(stack[i]+" ");
+		for(int i=top; i>=0; i--) {
+			System.out.println(stack[i]);
 		}
 	}
-	
+		
 	public static void main(String[] args) {
 		
 		Stack_array stack1 = new Stack_array();
@@ -46,17 +68,15 @@ public class Stack_array {
 		System.out.println("Stack after performing Push operation : ");
 		stack1.show();
 		
-		System.out.println("\nTop value of Stack after performing Push operation : "
-							+stack1.peek());
+		System.out.println("\nTop value of Stack after performing Push operation : "+stack1.peek());
 		
 		System.out.println("\nPopped value: "+stack1.pop());
 		
 		System.out.println("\nStack after performing Pop operation : ");
 		stack1.show();
 		
-		System.out.println("\nTop value of Stack after performing Pop operation : "
-							+stack1.peek());
+		System.out.println("\nTop value of Stack after performing Pop operation : "+stack1.peek());
+		
 	}
-	
 
 }
