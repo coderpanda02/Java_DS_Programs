424224b7eb9f8b78e1cfacd9a80bec19c466d156
Pratiti Basu <104024345+coderpanda02@users.noreply.github.com>
2024-05-16 01:03:55 +0530

    Update LinkedList1.java

--- Java_DS_Programs/src/testDSA/LinkedList1.java
+++ Java_DS_Programs/src/testDSA/LinkedList1.java
@@ -20,7 +20,7 @@ public class LinkedList1 {
 
 
 	private static LinkedList1 insert(LinkedList1 list, int data) {
-		
+	
 		Node new_node = new Node(data);
 		
 		if(list.head==null) {  //insert at head if LinkedList is empty
@@ -38,8 +38,7 @@ public class LinkedList1 {
 		return list;
 	}
 
-	private static LinkedList1 insert(LinkedList1 list, int data,
-										int position) {
+	private static LinkedList1 insert(LinkedList1 list, int data, int position) {
 		
 		Node new_node = new Node(data);
 		
@@ -54,6 +53,7 @@ public class LinkedList1 {
 			
 			//going to (n-1)th position   
 			for(int i=0;i<position-2;i++) { //i<(pos-2), not i<(pos-1) as counting from 0, so, pos=6 is actually index 5
+											
 				temp=temp.next; //Incrementing temp until (n-2)th pos (pos-3) and finally  
 						//pointing to (n-1)th pos (pos-2), so, i<pos-2, not i<=pos-2
 			}
@@ -68,6 +68,10 @@ public class LinkedList1 {
 	}
 	
 	private static LinkedList1 delete(LinkedList1 list,int position) {
+		
+		if(list.head == null) {
+			return null;
+		}
 		
 		Node del_temp=list.head;
 		
@@ -91,20 +95,77 @@ public class LinkedList1 {
 		return list;
 	}
 	
-	
-	
-	private void show(LinkedList1 list) {
+	private static Node deleteByVal(Node head, int i) {
 		
-		System.out.println("The list is: ");
-		
-		Node trv=list.head;
-		while(trv!=null) {
-			System.out.print(trv.data+" ");
-			trv=trv.next;
+		if(head == null) {
+			return null;
 		}
 		
+		if(head.data == i) {
+			head = deleteByVal(head.next, i);
+		}
+		else {
+			head.next = deleteByVal(head.next, i);
+		}
+		
+		return head;
+	}
+	
+	private static Node deleteByNode(Node head, Node delNode) {
+		
+		if(head == null) {
+			return null;
+		}
+		
+		if(delNode == null) {
+			System.out.println("Node to delete not found!");
+		}
+		
+		if(head == delNode) {
+			head = deleteByNode(head.next, delNode);
+		}
+		else {
+			head.next = deleteByNode(head.next, delNode);
+		}
+		
+		return head;
 	}
 
+	private static Node deleteMidNode(Node head) {
+		
+		if(head == null || head.next == null) {
+			return null;
+		}
+		
+		//two pointer approach using slow and fast (to avoid traversing the list more than once)
+		Node slow = new Node();
+		slow.next = head; //slow starts from the previous node of the head node
+		Node fast = head; //fast starts from the head node
+		
+		while(fast!=null && fast.next!=null) {
+			slow = slow.next; //slow advances once
+			fast = fast.next.next; //fast advances twice
+		}
+		//fast is now the last node, while slow is the previous node of the middle node
+		slow.next = slow.next.next; //deleting slow.next, i.e the middle node
+		
+		return head;
+	}
+
+	private void show(Node head) {
+		
+		if(head == null) {
+			System.out.println("The list is Empty!");
+		}
+		else {
+			System.out.println("The list is: ");
+			
+			while(head!=null) {
+				System.out.print(head.data+" ");
+				head = head.next;
+			}	
+		}	
+	}
 
 	public static void main(String[] args) {
 		
@@ -113,15 +174,55 @@ public class LinkedList1 {
 		list = insert(list, 2);
 		list = insert(list, 3);
 		list = insert(list, 4);
-		list.show(list);
-		System.out.println();
-		
-		list = insert(list, 5, 2);
-		list.show(list);
-		System.out.println();
-		
-		list = delete(list, 3);
-		list.show(list);
-	}
+		list = insert(list, 5);
+		list = insert(list, 6);
 
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nInsertVal : ");
+		list = insert(list, 10, 2);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteByPos : ");
+		list = delete(list, 3);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteByVal : ");
+		list.head = deleteByVal(list.head, 3);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteByNode : ");
+		list.head = deleteByNode(list.head, list.head.next.next);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteMidNode : ");
+		list.head = deleteMidNode(list.head);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteMidNode : ");
+		list.head = deleteMidNode(list.head);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteByVal : ");
+		list.head = deleteByVal(list.head, 1);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteByNode : ");
+		list.head = deleteByNode(list.head, list.head.next);
+		list.show(list.head);
+		System.out.println();
+		
+		System.out.println("\nAfter DeleteByNode : ");
+		list.head = deleteByNode(list.head, list.head);
+		list.show(list.head);
+
+	}
 }
