c4cd8d51ff79f4002ba624b5eb6ec2a927f2281a
Pratiti Basu <104024345+coderpanda02@users.noreply.github.com>
2024-05-22 23:38:59 +0530

    Update LinkedList3_Cycle.java

--- Java_DS_Programs/src/testDSA/LinkedList3_Cycle.java
+++ Java_DS_Programs/src/testDSA/LinkedList3_Cycle.java
@@ -1,1 +1,6 @@
-//If a node in the linked list can be accessed again by continuously following the next pointer, there is a Cycle or Loop in the Linked List.
+// If a node in the linked list can be accessed again by continuously following the next pointer, there is a Cycle or Loop in the Linked List.
+// To find the cycle using Floyd's Cycle-Finding algorithm (Also called Hare-Tortoise algorithm) ->
+// Taking Two pointers of different speed - slow (slow = slow.next) and fast (fast = fast.next.next) pointers.
+// If slow and fast meets, there is a cycle. Though it might not be the starting point of the cycle, to find that we have to
+// reset the slow pointer to head again (keeping fast as it is) and increment both by one node this time (slow = slow.next, fast = fast.next)
+// until they meet again. Where they meet for the 2nd time, that is the starting point of the cycle. 
